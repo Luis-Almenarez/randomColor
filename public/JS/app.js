3fd78ba7227153fab28b55c18dec84638e912426
Luis Almenarez <luisalmenarez0428@gmail.com>
2023-09-14 20:11:53 -0500

    Initial commit

---
+++ public/JS/app.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector(".btn");
+const color = document.querySelector("#color");
+const codeColor = document.querySelector("#code-color");
+const body = document.querySelector(".body");
+
+function colorGenerate() {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  const newColor = "#" + randomColor;
+  codeColor.innerHTML = newColor;
+  color.style.backgroundColor = newColor;
+}
+
+btn.addEventListener("click", colorGenerate);
